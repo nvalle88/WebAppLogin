6a38096fc076c56e8ce0d6e95b4f7fc207940464
Carlos Avila <carlos.avila8909@gmail.com>
2018-06-19 15:43:42 -0400

    Se agregaró la implementación para guardar los Claims IdSucursal y NombreSucursal para el usuario que se autentica.

---
+++ WebAppLoginFinal/bd.webapplogin.entidades/ObjectTransfer/Sucursal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace bd.webapplogin.entidades.ObjectTransfer
+{
+    public class Sucursal
+    {
+        public int IdSucursal { get; set; }
+        public string Nombre { get; set; }
+    }
+}
--- WebAppLoginFinal/bd.webapplogin.entidades/Utils/WebApp.cs
+++ WebAppLoginFinal/bd.webapplogin.entidades/Utils/WebApp.cs
@@ -7,7 +7,7 @@ namespace bd.webappth.entidades.Utils
    public static class WebApp
     {
         public static string BaseAddressSeguridad { get; set; }
-
+        public static string BaseAddressTH { get; set; }
         public static string NombreAplicacion { get; set; }
         public static string BaseAddressWebAppLogin { get; set; }
     }
